8dda619dc711632979d7a93e61fb6168c6fb061d
student <manjaka.ran@onja.org>
2021-11-30 11:06:36 +0300

    removes console log and changes the button text

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,6 @@ const Home = () => {
     variables: gqlVariables,
   })
   const [openModal, setOpenModal] = useState(false)
-  console.log(data)
   return (
     <div className='flex flex-col p-4'>
       <div className='flex justify-end'>
@@ -29,7 +28,7 @@ const Home = () => {
           onClick={() => setOpenModal(true)}
           type='button'
           className='inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-          Create Launches
+          Create a new Launch
         </button>
       </div>
       <div className='flex flex-col pt-5'>
